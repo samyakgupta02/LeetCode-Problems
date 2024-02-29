ed931ce9ec46b2aa0fbd4835cf658f8f3393997c
Samyak Gupta <samyakgupta02@gmail.com>
2024-02-29 16:01:26 +0530

    files added

---
+++ Kadane_algo/src/Main.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+class BruteForce {
+    public static int maxSubarraySum(int[] arr, int n) {
+        int maxi = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                int sum = 0;
+
+
+                for (int k = i; k <= j; k++) {
+                    sum += arr[k];
+                }
+
+                maxi = Math.max(maxi, sum);
+            }
+        }
+
+        return maxi;
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int n = arr.length;
+        int maxSum = maxSubarraySum(arr, n);
+        System.out.println("The maximum subarray sum is: " + maxSum);
+
+    }
+
+}
